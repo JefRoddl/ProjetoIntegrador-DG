efe6c96f6c630be4527e2810a4c35775dbd01752
Jeferson <Jefersonroddl@gmail.com>
2023-05-01 18:59:00 -0300

    nome do Adm aparece em todas as páginas

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const path = require("path");
 const app = express();
 const router =  require('./router.js')
 const bodyParser = require('body-parser')
+const session = require('express-session');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'my-secret-key',
+    resave: false,
+    saveUninitialized: false
+  }));
 
 
 //Define a pasta public como sendo a pasta de arquivos estáticos
--- src/controllers/administracaoController.js
+++ src/controllers/administracaoController.js
@@ -1,32 +1,58 @@
 const dadosloginAdm = require('../infra/loginAdm.json')
 
-
 const administracao = {
-    enviarLogin: (req, res) => {
-      const emailAdm = req.body.emailAdm;
-      const senhaAdm = req.body.senhaAdm;
-  
-      // Procura um usuário administrador que tenha o mesmo e-mail fornecido pelo usuário
-      const usuarioAdm = dadosloginAdm.usuarios.find(usuario => usuario.email === emailAdm);
-  
-      if (usuarioAdm && usuarioAdm.senha === senhaAdm) {
+  enviarLogin: (req, res) => {
+    const emailAdm = req.body.emailAdm;
+    const senhaAdm = req.body.senhaAdm;
+
+    // Procura um usuário administrador que tenha o mesmo e-mail fornecido pelo usuário
+    const usuarioAdm = dadosloginAdm.usuarios.find(usuario => usuario.email === emailAdm);
+
+    if (usuarioAdm && usuarioAdm.senha === senhaAdm) { 
+        // armazena o nome do usuário na sessão
+        req.session.usuarioAdmNome = usuarioAdm.nome;
+        req.session.usuarioAdmSobrenome = usuarioAdm.sobrenome;
         res.redirect('/produtos-adm');
-      } else {
+    } else {
         res.send('E-mail ou senha inválidos.');
-      }
-    },
+    }
+},
+
+
     showLoginAdm: (req, res) => {
         res.render('login-adm', {dadosloginAdm})
     },
+
+
+
     showClientesAdm: (req, res) => {
-        res.render('clientes-adm', {dadosloginAdm})
+        const usuarioAdmNome = req.session.usuarioAdmNome;
+        const usuarioAdmSobrenome = req.session.usuarioAdmSobrenome
+        res.render('clientes-adm', {usuarioAdmNome, usuarioAdmSobrenome});
+        
+      },
+
+
+
+
+      showProdutosAdm: (req, res) => {
+        const usuarioAdmNome = req.session.usuarioAdmNome;
+        const usuarioAdmSobrenome = req.session.usuarioAdmSobrenome
+        res.render('produtos-adm', { usuarioAdmNome, usuarioAdmSobrenome });
     },
-    showProdutosAdm: (req, res) => {
-        res.render('produtos-adm', {dadosloginAdm})
-    },
+
+
+
+
     showPedidosAdm: (req, res) => {
-        res.render('pedidos-adm', {dadosloginAdm})
+        const usuarioAdmNome = req.session.usuarioAdmNome;
+        const usuarioAdmSobrenome = req.session.usuarioAdmSobrenome
+        res.render('pedidos-adm', {usuarioAdmNome, usuarioAdmSobrenome})
     },
+
+
+
+
 }
 
 module.exports = administracao
